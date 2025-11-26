0aca188dc145dbfe65890847f1b39ec582f311bc
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2025-11-26 12:10:59 +0100

    Move codeQualityCheck to code-quality.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+plugins {
+    id("org.jlleitschuh.gradle.ktlint") version "14.0.1" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.8" apply false
+}
+
 buildscript {
     val kotlinVersion = "2.2.21"
     val gradleVersion = "8.13.1"
@@ -7,11 +12,15 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:$gradleVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin.plugin.compose:org.jetbrains.kotlin.plugin.compose.gradle.plugin:$kotlinVersion")
+        // central code-quality plugin classpaths so applied script can use apply(plugin=...)
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:14.0.1")
+        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.8")
     }
 }
 
@@ -27,40 +36,4 @@ tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
 
-plugins {
-    id("org.jlleitschuh.gradle.ktlint") version "13.1.0" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.23.8" apply false
-}
-
-subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-    apply(plugin = "io.gitlab.arturbosch.detekt")
-
-    // Configure KtLint after the plugin is applied
-    extensions.findByType(org.jlleitschuh.gradle.ktlint.KtlintExtension::class.java)?.apply {
-        android.set(true)
-        ignoreFailures.set(false)
-    }
-
-    // Configure Detekt after the plugin is applied
-    extensions.findByType(io.gitlab.arturbosch.detekt.extensions.DetektExtension::class.java)
-        ?.apply {
-            buildUponDefaultConfig = true
-            allRules = true
-            ignoreFailures = false
-            // Optional config file
-            // config.setFrom(rootDir.resolve("config/detekt/detekt.yml"))
-        }
-}
-
-// Root-level task to run both checks
-tasks.register("codeQualityCheck") {
-    dependsOn(subprojects.flatMap {
-        listOf(
-            it.tasks.named("ktlintCheck"),
-            it.tasks.named("detekt")
-        )
-    })
-}
-
-//apply(from = "code-quality.gradle.kts")
+apply(from = "code-quality.gradle.kts")
---
+++ code-quality.gradle.kts
@@ -0,0 +1,39 @@
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    afterEvaluate {
+        fun configureExtension(name: String, vararg methods: Pair<String, Any>) {
+            extensions.findByName(name)?.runCatching {
+                val cls = javaClass
+                methods.forEach { (method, value) ->
+                    cls.getMethod(method, value::class.java).invoke(this, value)
+                }
+            }
+        }
+
+        configureExtension(
+            "ktlint",
+            "setAndroid" to true,
+            "setIgnoreFailures" to false
+        )
+
+        configureExtension(
+            "detekt",
+            "setBuildUponDefaultConfig" to true,
+            "setAllRules" to true,
+            "setIgnoreFailures" to false
+        )
+    }
+}
+
+tasks.register("codeQualityCheck") {
+    dependsOn(
+        subprojects.flatMap {
+            listOf(
+                it.tasks.named("ktlintCheck"),
+                it.tasks.named("detekt")
+            )
+        }
+    )
+}
