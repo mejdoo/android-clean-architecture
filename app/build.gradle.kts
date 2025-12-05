dca54604af8c4921af68c349e83f2af8dac0f5d4
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2025-12-05 21:30:52 +0100

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,26 +75,27 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
     // Android Architecture Components
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
-    kapt("androidx.lifecycle:lifecycle-compiler:2.9.4")
-    implementation("androidx.room:room-runtime:2.8.3")
-    kapt("androidx.room:room-compiler:2.8.3")
-    implementation("androidx.room:room-ktx:2.8.3")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    kapt("androidx.lifecycle:lifecycle-compiler:2.10.0")
+    implementation("androidx.room:room-runtime:2.8.4")
+    kapt("androidx.room:room-compiler:2.8.4")
+    implementation("androidx.room:room-ktx:2.8.4")
+    implementation("androidx.core:core-ktx:1.17.0")
 
     // Koin (Dependency injection framework for Kotlin)
     implementation("io.insert-koin:koin-android:4.1.1")
 
     // Jetpack Compose
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation("androidx.compose.ui:ui:1.5.0")
-    implementation("androidx.compose.material:material:1.5.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
+    implementation("androidx.activity:activity-compose:1.12.1")
+    implementation("androidx.compose.ui:ui:1.10.0")
+    implementation("androidx.compose.material:material:1.10.0")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.10.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.10.0")
 
     // Coil for Compose (Image Loading)
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
     // Unit testing
     testImplementation("junit:junit:4.13.2")
